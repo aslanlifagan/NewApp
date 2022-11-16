f7f347dfd118eb0ba741f02e1dc3fa6745adfa48
Fagan Aslanli <aslanli9696@gmail.com>
2022-11-16 23:27:54 +0400

    Create NibLoadable.swift

---
+++ Helpers/NibLoadable.swift
@@ -0,0 +1,35 @@
+//
+//  NibLoadable.swift
+//  NewApp
+//
+//  Created by Fagan Aslanli on 14.11.22.
+//
+
+import UIKit
+import SnapKit
+
+public protocol NibLoadable {
+    static var nibName: String { get }
+}
+
+public extension NibLoadable where Self: UIView {
+
+    static var nibName: String {
+        return String(describing: Self.self) // defaults to the name of the class implementing this protocol.
+    }
+
+    static var nib: UINib {
+        let bundle = Bundle(for: Self.self)
+        return UINib(nibName: Self.nibName, bundle: bundle)
+    }
+
+    func setupFromNib() {
+        guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+            fatalError("Error loading \(self) from nib")
+        }
+        addSubview(view)
+        view.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+    }
+}
